4f1ff632ab5ba9825e472695f34b7a6e70cc3fce
PraveenRathore001 <praveenrathore236@gmail.com>
2023-04-12 14:16:15 +0530

    kai ni aese he

---
+++ example.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const logger = require("./logger");
+const PORT = process.env.PORT || "5555";
+const port = process.env.port || "5556";
+
+const app = express();
+const app1 = express();
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    logger.log("debug", "Hello, World!"); //debug level as first param
+    logger.debug("The is the home '/' route.");
+    // using debug method directly
+    res.send( {msg:"Hello World" });
+});
+
+app.get('/404', (req, res) => {
+    logger.error("404 error"); //error method
+    logger.debug("The is the 404 route.");
+    res.sendStatus(404);
+})
+
+app.get("/user", (req, res) => {
+    try {
+      throw new Error("Invalid user");
+    } catch (error) {
+      logger.error("Auth Error: invalid user");
+      logger.debug("The is the user route.");
+      res.status(500).send("Error!");
+    }
+  });
+
+app.listen(parseInt(PORT,10), () => {
+
+    console.log(`Listening on http://localhost:${PORT}`);
+});
+app.listen(parseInt(port, 10), () => {
+    console.log(`Listening on http://localhost:${port}`);
+});
